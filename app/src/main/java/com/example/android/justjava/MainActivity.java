7c3ae45c5d516b7be9f0614eb5e6bc6fe2e28039
Volodymyr Tkachenko <thechenko@gmail.com>
2018-02-21 15:32:16 +0000

    Added email functionality and removed order summary from xml

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.android.justjava;
 
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -26,19 +30,42 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        TextView oderSummaryTextView = findViewById(R.id.order_summary_text_view);
-        int price = calculatePrice();
-        oderSummaryTextView.setText(createOrderSummary(price));
+        CheckBox wc = findViewById(R.id.wc_checkbox);
+        CheckBox ch = findViewById(R.id.choc_checkbox);
+        EditText ed = findViewById(R.id.username);
+        String name = ed.getText().toString();
+        boolean hasChocolate = ch.isChecked();
+        boolean hasWhippedCream = wc.isChecked();
+
+        int price = calculatePrice(hasChocolate, hasWhippedCream);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, createOrderSummary(price, hasWhippedCream, hasChocolate, name));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
-    private int calculatePrice() {
-        int price = quantity * 5;
+    private int calculatePrice(boolean chocolate, boolean cream) {
+        int ppc = 5;
+
+        if(chocolate){
+            ppc = ppc + 1;
+        }
+        if (cream) {
+                ppc = ppc + 2;
+        }
+        int price = quantity * ppc;
+
         return price;
     }
 
-    private String createOrderSummary(int price){
-        String name = "Vova Checnko";
+    private String createOrderSummary(int price, boolean whippedCream, boolean chocolate, String name){
         String msg = "Name: " + name +
+                    "\nAdd whipped cream? " + whippedCream +
+                    "\nAdd chocolate?" + chocolate +
                     "\nQuantity: " + quantity +
                     "\nTotal: £" + price +
                     "\nThank you!";
@@ -50,6 +77,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void increment(View view) {
+        if (quantity == 100){
+            Toast.makeText(this, "Cannot go above 100", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity = quantity + 1;
         displayQuantity(quantity);
     }
@@ -58,8 +89,13 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void decrement(View view) {
+        if (quantity == 1){
+            Toast.makeText(this, "Cannot go below 1", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity = quantity - 1;
         displayQuantity(quantity);
+
     }
 
     /**
@@ -67,6 +103,6 @@ public class MainActivity extends AppCompatActivity {
      */
     private void displayQuantity(int numberOfCoffees) {
         TextView quantityTextView = findViewById(R.id.quantity_text_view);
-        quantityTextView.setText(numberOfCoffees);
+        quantityTextView.setText("" + numberOfCoffees);
     }
 }
